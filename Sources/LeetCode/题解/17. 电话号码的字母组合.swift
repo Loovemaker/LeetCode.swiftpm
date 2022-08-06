60a2225db98eb7d77f2e8342c3d57c32b145fbec
Loovemaker <bows-02.roper@icloud.com>
2022-08-06 13:49:25 +0800

    solved 17. 电话号码的字母组合

---
+++ Sources/LeetCode/题解/17. 电话号码的字母组合.swift
@@ -0,0 +1,35 @@
+public extension Solution {
+    
+    /// <doc:17.---------->
+    ///
+    /// - ⌛️：82%
+    /// - 🫙：58%
+    /// - Parameter digits: 电话号码数字的数组
+    /// - Returns: 所有字符串组合（不排序）
+    func letterCombinations(_ digits: String) -> [String] {
+        var result = [""]
+        
+        for digitChar in digits {
+            let digit = Int(String(digitChar))!
+            let alphabets = mapping[digit]!
+            
+            result = result.cartesianProduct(with: alphabets, productMethod: +)
+        }
+        
+        if result == [""] { result = [] }
+//        result.removeAll { $0.isEmpty }
+        
+        return result
+    }
+}
+
+fileprivate let mapping = [
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "x", "y", "z"],
+]
---
+++ Tests/LeetCodeTests/测试用例/17. 电话号码的字母组合.swift
@@ -0,0 +1,20 @@
+import XCTest
+@testable import LeetCode
+
+extension LeetCodeTests {
+    
+    func test_letterCombinations() {
+        typealias Input = String
+        typealias Output = Set<String>
+        
+        @CPBuilder<Input, Output> var casePairs: [CasePair<Input, Output>] {
+            ("23",  ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+            ("",    [])
+            ("2",   ["a", "b", "c"])
+        }
+        
+        casePairs.xctAssertEqual {
+            Set(solution.letterCombinations($0))
+        }
+    }
+}
