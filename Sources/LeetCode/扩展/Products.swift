8b810f422e93f674413fa7b5d7e8c83489f594a5
Loovemaker <bows-02.roper@icloud.com>
2022-08-17 02:42:10 +0800

    add return type of cartesianProduct

--- Sources/LeetCode/扩展/Products.swift
+++ Sources/LeetCode/扩展/Products.swift
@@ -3,7 +3,7 @@ extension Collection {
     /// - Parameters:
     ///   - other: 另一个`Collection`
     ///   - productMethod: 积的方法
-    /// - Returns: 笛卡尔积结果
+    /// - Returns: 笛卡尔积结果（`Array`）
     func cartesianProduct<Other, ResultElement>(
         with other: Other,
         productMethod: (Element, Other.Element) -> ResultElement
@@ -20,4 +20,25 @@ extension Collection {
         return result
     }
     
+    /// 笛卡尔积
+    /// - Parameters:
+    ///   - other: 另一个`Collection`
+    ///   - productMethod: 积的方法
+    /// - Returns: 笛卡尔积结果（`some Collection`）
+    @available(macOS 10.15.0, *)
+    func cartesianProduct<Other, ResultElement>(
+        with other: Other,
+        productMethod: (Element, Other.Element) -> ResultElement
+    ) -> some Collection
+    where Other: Collection {
+        var result: [ResultElement] = []
+        
+        for lElement in self {
+            for rElement in other {
+                result.append(productMethod(lElement, rElement))
+            }
+        }
+        
+        return result
+    }
 }
